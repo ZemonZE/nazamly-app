f78fbcf3391935323ddcd8f8f6cf67aeaa18fc99
Mohamed-walid88 <mohamedwa904@gmail.com>
2026-02-26 23:14:47 +0200

    Time Table Entry model

---
+++ nazamly-backend/src/models/timeTableEntry.model.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const timeTableEntrySchema = new Schema({
+    timeTableId: {
+        type: Schema.Types.ObjectId,
+        ref: 'TimeTable',
+        required: [true, 'TimeTable is required'],
+        index: true
+    },
+    courseId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Course',
+        required: [true, 'Course is required'],
+        index: true
+    },
+    dayOfWeek: {
+        type: Number,
+        required: [true, 'Day of week is required'],
+        min: [0, 'Day of week must be between 0 (Sunday) and 6 (Saturday)'],
+        max: [6, 'Day of week must be between 0 (Sunday) and 6 (Saturday)'],
+        validate: {
+            validator: Number.isInteger,
+            message: 'Day of week must be an integer'
+        }
+    },
+    startTime: {
+        type: String,
+        required: [true, 'Start time is required'],
+        match: [/^([01]\d|2[0-3]):([0-5]\d)$/, 'Start time must be in "HH:mm" format (24-hour)']
+    },
+    endTime: {
+        type: String,
+        required: [true, 'End time is required'],
+        match: [/^([01]\d|2[0-3]):([0-5]\d)$/, 'End time must be in "HH:mm" format (24-hour)'],
+        
+        // Custom validator to ensure end time is after start time
+        validate: {
+            validator: function (value) {
+                const [startHour, startMin] = this.startTime.split(':').map(Number);
+                const [endHour, endMin] = value.split(':').map(Number);
+                const startMinutes = startHour * 60 + startMin;
+                const endMinutes = endHour * 60 + endMin;
+                return endMinutes > startMinutes;
+            },
+            message: 'End time must be after start time'
+        }
+    },
+    location: {
+        type: String,
+        required: false,
+        trim: true,
+        maxlength: [100, 'Location cannot exceed 100 characters']
+    }
+},
+{
+    timestamps: true,
+    collection: 'timeTableEntries'
+});
+
+module.exports = model("TimeTableEntry", timeTableEntrySchema);
