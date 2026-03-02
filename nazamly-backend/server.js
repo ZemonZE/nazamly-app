3e0771acde09e3b13baa67cf5873a13379e022b4
AbdelrahmanOsama243 <Body16005@gmail.com>
2026-03-02 22:28:28 +0200

    singelton_DB_connetion

--- nazamly-backend/server.js
+++ nazamly-backend/server.js
@@ -2,7 +2,10 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
-connectDB();
+connectDB().catch((error) => {
+  console.error("Failed to connect to the database:", error);
+  process.exit(1); // Exit with failure code
+});
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
--- nazamly-backend/src/config/db.js
+++ nazamly-backend/src/config/db.js
@@ -1,10 +1,34 @@
 const mongoose = require("mongoose");
 
+let connection = null;
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+try{  
+  if (connection) {
+    return connection;
+  }
+
+  connection = await mongoose.connect("mongodb://localhost:27017/nazamly");
   console.log("Mongo Connected");
+  return connection;
+} catch (error) {
+  console.error("MongoDB connection error:", error);
+}
 };
 
-module.exports = connectDB;
+const getConnection = () => {
+  try{
+  if (!connection) {
+    throw new Error("Database has not been connected yet. Call connectDB() first.");
+  }
+  return connection;
+} catch (error) {
+  console.error("MongoDB connection error:", error);
+}
+};
+
+module.exports = {
+  connectDB,
+  getConnection,
+};
 
 //اخو فاير بيز هو مونجو نفس الفكرة
