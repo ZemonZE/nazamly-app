4df59e2004c9387cadf1988c30c07b0874a9940a
Amr Mahmoud <10812023100482@stud.cu.edu.eg>
2026-03-05 23:18:25 +0200

    feat: add dashboard, GPA calculator, GPA planner, schedule generator, and settings pages with dark mode

---
+++ nazamly-front/src/pages/Questions.jsx
@@ -0,0 +1,16 @@
+function Questions() {
+  return (
+    <div
+      className="dash-home"
+      style={{
+        textAlign: "center",
+        color: "white",
+        fontSize: "5rem",
+      }}
+    >
+      Coming Soon
+    </div>
+  );
+}
+export default Questions;
+
