e06e964f64185c9b9df5d9fea3026bc3442d9126
AbdelrahmanOsama243 <body162005@gmail.com>
2026-03-06 23:10:34 +0200

    =

--- nazamly-backend/src/config/db.js
+++ nazamly-backend/src/config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URL, {
       dbName: "nazamly-app",
     });
 
