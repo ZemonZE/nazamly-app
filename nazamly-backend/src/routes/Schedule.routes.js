7701212641795378ec607e3bb36f15f9d7dc4134
AbdelrahmanOsama243 <body162005@gmail.com>
2026-03-07 02:47:00 +0200

    try 110

--- nazamly-backend/src/routes/Schedule.routes.js
+++ nazamly-backend/src/routes/Schedule.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const requireAuth = require("../middlewares/auth.middleware");
+
+const authMiddleware = require("../middlewares/auth.middleware");
 const {
   addOrUpdateSchedule,
   getMySchedule,
